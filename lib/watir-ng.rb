f1940aba0ff17579523631736f0f1a73532c0031
Aaron Humerickhouse <aaron@zipnosis.com>
2016-12-30 10:43:10 -0600

    changing to watir

--- lib/watir-ng.rb
+++ lib/watir-ng.rb
@@ -1,4 +1,4 @@
-require "watir-webdriver"
+require "watir"
 require "watir-ng/version"
 
 #
@@ -6,7 +6,7 @@ require "watir-ng/version"
 #
 module WatirNg
   class << self
-    
+
     #
     # Register custom directives as element identifiers.
     #
@@ -43,13 +43,13 @@ module WatirNg
     # @return [Array<Symbol>]
     #
     def directives
-      %w(ng_jq ng_app ng_href ng_src ng_srcset ng_disabled ng_checked ng_readonly 
-         ng_selected ng_open ng_form ng_value ng_bind ng_bind_template ng_bind_html 
-         ng_change ng_class ng_class_odd ng_class_even ng_cloak ng_controller ng_csp 
-         ng_click ng_dblclick ng_mousedown ng_mouseup ng_mouseover ng_mouseenter 
+      %w(ng_jq ng_app ng_href ng_src ng_srcset ng_disabled ng_checked ng_readonly
+         ng_selected ng_open ng_form ng_value ng_bind ng_bind_template ng_bind_html
+         ng_change ng_class ng_class_odd ng_class_even ng_cloak ng_controller ng_csp
+         ng_click ng_dblclick ng_mousedown ng_mouseup ng_mouseover ng_mouseenter
          ng_mouseleave ng_mousemove ng_keydown ng_keyup ng_keypress ng_submit ng_focus
-         ng_blur ng_copy ng_cut ng_paste ng_if ng_include ng_init ng_list ng_model 
-         ng_model_options ng_non_bindable ng_options ng_pluralize ng_repeat ng_show 
+         ng_blur ng_copy ng_cut ng_paste ng_if ng_include ng_init ng_list ng_model
+         ng_model_options ng_non_bindable ng_options ng_pluralize ng_repeat ng_show
          ng_hide ng_style ng_switch ng_transclude).map(&:to_sym)
     end
 
@@ -62,6 +62,6 @@ module WatirNg
     def custom_directives
       @custom_directives ||= []
     end
-    
+
   end
 end
--- lib/watir-ng/version.rb
+++ lib/watir-ng/version.rb
@@ -1,4 +1,4 @@
 module WatirNg
   # :nodoc:
-  VERSION = "2.0.0"
+  VERSION = "3.0.0"
 end
